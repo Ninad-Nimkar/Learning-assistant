fa0aa022f12d948b493dc9140db675520ad5c974
Gudboy <ninadnimkar2021@gmail.com>
2026-02-19 22:42:41 +0530

    summarizer

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+class ExplainRequest(BaseModel):
+    text: str
+
+@app.get("/")
+def root():
+    return {"message": "AI Explainer Backen Running"}
+
+@app.post("/explain")
+def explain(request:ExplainRequest):
+
+    prompt = f"""
+    you are helpful educatrional assistant,
+    Explain the following topic clearly and simply.
+
+    Topic: {request.text}
+    """
+        
+    response = requests.post(
+        "https://api.sarvam.ai/v1/chat/completions",
+        headers = {
+            "api-subscription-key": os.getenv("SARVAM_API_KEY")
+        },
+        json={
+            "messages":[
+                {
+                    "role": "user", 
+                    "content": prompt
+                }
+            ],
+            "model": "sarvam-m"
+        }
+    )
+    
+    return (response.json()["choices"][0]["message"]["content"])
