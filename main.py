9faf0441ef151efde310093c093f756794a47b7b
Gudboy <ninadnimkar2021@gmail.com>
2026-02-21 09:55:17 +0530

    Implemented OCR

--- main.py
+++ main.py
@@ -3,27 +3,38 @@ from pydantic import BaseModel
 import requests
 import os
 from dotenv import load_dotenv
+from fastapi import UploadFile, File
+from sarvamai import SarvamAI
+import pdfplumber
+import pytesseract
+from PIL import Image
+import io
 
 load_dotenv()
+SARVAM_API_KEY = os.getenv("SARVAM_API_KEY")
+client = SarvamAI(api_subscription_key=SARVAM_API_KEY) 
 
 app = FastAPI()
 
-class ExplainRequest(BaseModel):
-    text: str
-    style: str = "simple"
+@app.post("/upload-and-explain")
+async def upload_and_explain(file: UploadFile = File(...), style = "simple"):
 
-@app.get("/")
-def root():
-    return {"message": "AI Explainer Backen Running"}
+    file_bytes = await file.read()
+    extracted_text = ""
 
-@app.post("/explain")
-def explain(request:ExplainRequest):
+    #extract text
+    if file.filename.endswith(".pdf"):
+        with pdfplumber.open(io.BytesIO(file_bytes)) as pdf:
+            for page in pdf.pages:
+                extracted_text += page.extract_text() or ""
+
+    extracted_text = extracted_text[:4000]
 
     prompt = f"""
     you are helpful educatrional assistant,
     Explain the following topic clearly and simply.
     
-    Explain the following topic in {request.style} style.
+    Explain the following topic in {style} style.
 
     Make it:
     - Clear
@@ -31,23 +42,19 @@ def explain(request:ExplainRequest):
     - Easy to understand
     - Suitable for Indian students
 
-    Topic: {request.text}
+    Topic: {extracted_text}  
     """
         
-    response = requests.post(
-        "https://api.sarvam.ai/v1/chat/completions",
-        headers = {
-            "api-subscription-key": os.getenv("SARVAM_API_KEY")
-        },
-        json={
-            "messages":[
-                {
-                    "role": "user", 
-                    "content": prompt
-                }
-            ],
-            "model": "sarvam-m"
-        }
+            
+    chat_response = client.chat.completions(
+        messages=[{
+            "content": prompt,
+            "role": "user"
+        }]
     )
-    
-    return (response.json()["choices"][0]["message"]["content"])
+        
+    explanation = chat_response.choices[0].message.content
+    return {
+        "extracted_text": extracted_text[:4000],
+        "explanation": explanation
+    }
